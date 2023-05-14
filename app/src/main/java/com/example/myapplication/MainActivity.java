5025d3d464f22e43862c4e59458fb8050a25bbe9
FatiId04 <fatimaalzahraidrissi03@gmail.com>
2023-05-14 23:09:50 +0100

    Initial Commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
 
         username=findViewById(R.id.input1);
         password=findViewById(R.id.input2);
